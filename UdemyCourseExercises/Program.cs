6b891be61b16e1feb7bb65b2687f981fad4140c8
Tyler Barlow <barlowtyler96@gmail.com>
2023-05-11 17:06:51 -0400

    Finished all exercises

--- UdemyCourseExercises/Program.cs
+++ UdemyCourseExercises/Program.cs
@@ -1,4 +1,6 @@
-﻿//These exercises are part of a course I'm taking on Udemy (Mosh Hamedani's Basic and Intermediate).
+﻿using System.Globalization;
+
+//These exercises are part of a course I'm taking on Udemy (Mosh Hamedani's Basic and Intermediate).
 //Obviously these are not real world applications, so I did not always validate input or handle exceptions unless specified.
 //You can uncomment the code to run any exercise(ctrl+K+U).
 
@@ -395,13 +397,102 @@
 
 
 //==================================================================================================
-//#15 Arrays and Lists
-//      Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if
-//      the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16",
-//      display a message: "Consecutive"; otherwise, display "Not Consecutive".
-Console.WriteLine("Enter a few numbers separated by hypen (eg.'5-6-7-8-9') or ('20-19-18-17-16')");
-var input = Console.ReadLine();
+//#15 Working with text
+//     Write a program and ask the user to enter a few numbers separated by a hyphen. If the user
+//     simply presses Enter, without supplying an input, exit immediately; otherwise, check to see
+//     if there are duplicates. If so, display "Duplicate" on the console.
+//Console.WriteLine("Enter a few numbers separated by hypen (eg.'5-6-7-8-9') or ('20-19-18-17-16')");
+//var input = Console.ReadLine();
 
-var numbers = new List<int>();
-foreach (var number in input.Split('-'))
-    input.Add(Convert.ToInt32(number));
+//if (string.IsNullOrWhiteSpace(input))
+//{
+//    Environment.Exit(0);
+//}
+//var numbersArray = input.Split('-');
+//var numbers = new List<int>();
+
+//foreach (var number in numbersArray)
+//{
+//    numbers.Add(Convert.ToInt32(number));
+//}
+
+//var uniques = new List<int>(); 
+//bool duplicates = false;
+
+//foreach (var number in numbers)
+//{
+//    if (uniques.Contains(number) == false)
+//    {
+//        uniques.Add(number);
+//    }
+//    else
+//    {
+//        duplicates = true;
+//        break;
+//    }
+
+//}
+//if (duplicates == true)
+//{
+//    Console.WriteLine("Duplicates");
+//}
+//==================================================================================================
+//
+
+
+
+
+
+//==================================================================================================
+//#16 Working with text
+//      Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+//      A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise,
+//      display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+//Console.WriteLine("Enter a time in 24-hour format (eg. 19:00)");
+//var input = Console.ReadLine();
+//if (DateTime.TryParseExact(input, "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+//{
+//    Console.WriteLine("Ok");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid time.");
+//}
+//==================================================================================================
+
+
+
+
+
+
+//==================================================================================================
+//#17 Working with text
+//      Write a program and ask the user to enter a few words separated by a space. Use the words to
+//      create a variable name with PascalCase. For example, if the user types: "number of students",
+//      display "NumberOfStudents". Make sure that the program is not dependent on the input. So, if
+//      the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+//Console.WriteLine("Enter a few words separated by space: ");
+//var input = Console.ReadLine();
+
+//if (string.IsNullOrWhiteSpace(input))
+//{
+//    Console.WriteLine("Invalid input");
+//    Environment.Exit(0);
+//}
+
+//var wordsArray = input.Split(' ');
+//string pascalVariable = "";
+
+//foreach (var word in wordsArray)
+//{
+//    var pascalWord = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+//    pascalVariable += pascalWord;
+//}
+//Console.WriteLine(pascalVariable);
+//==================================================================================================
+
+
+
+
+
+
